a8b6b031f44c9c7a9ec7d399dc08233620286c5c
6.9 <james532@tutamail.com>
2025-09-24 14:40:45 +0200

    Update bootsrap.sh

--- bootsrap.sh
+++ bootsrap.sh
@@ -29,13 +29,9 @@ if [[ "$SHELL" != "/bin/bash" ]]; then
   echo "Shell changed to bash. You may need to log out and log back in."
 fi
 
-if [ ! -d "$HOME/ble.sh" ]; then
-  git clone --recursive https://github.com/akinomyoga/ble.sh.git "$HOME/ble.sh"
-  cd "$HOME/ble.sh"
-  make
-  make install
-  cd "$DOTFILES_DIR"
-fi
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source -- ~/.local/share/blesh/ble.sh' >> ~/.bashrc
 
 # --- install offsec tools ---
 sudo apt install -y python3-pip pypy3-venv python3.13-venv odat ssh-audit enum4linux-ng /
